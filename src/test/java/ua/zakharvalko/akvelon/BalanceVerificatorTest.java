2f31758d61bc96efdc06d88d725c74e502407a0a
Zakhar <zahar.valko@gmail.com>
2021-07-04 17:44:32 +0300

    Added 3 more tests for StringChecker class

---
+++ src/test/java/ua/zakharvalko/akvelon/BalanceVerificatorTest.java
@@ -0,0 +1,9 @@
+package ua.zakharvalko.akvelon;
+
+import org.mockito.Mockito;
+
+public class BalanceVerificatorTest {
+
+    StringChecker stringChecker = Mockito.mock(StringChecker.class);
+
+}
--- src/test/java/ua/zakharvalko/akvelon/StringCheckerTest.java
+++ src/test/java/ua/zakharvalko/akvelon/StringCheckerTest.java
@@ -2,26 +2,49 @@ package ua.zakharvalko.akvelon;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class StringCheckerTest {
 
     StringChecker stringChecker = new StringChecker();
+    String input = "([{}])";
 
     @Test
-    public void shouldReturnTrueIfStringNotEmpty() {
-        String input = "([{}])";
+    @DisplayName("Should return true if string is not empty")
+    public void checkForEmpty_nominal() {
         Assert.assertTrue(stringChecker.checkForEmpty(input));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    @DisplayName("Should throw IllegalArgumentException if string is empty")
+    public void checkForEmpty_exception() {
+        String input = "";
+        stringChecker.checkForEmpty(input);
+    }
+
     @Test
-    public void shouldReturnTrueIfStringHasOnlyBrackets() {
-        String input = "([{}])";
+    @DisplayName("Should return true if string has only brackets")
+    public void checkForCorrectness_nominal() {
+        Assert.assertTrue(stringChecker.checkForCorrectness(input));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @DisplayName("Should throw IllegalArgumentException if string has not brackets")
+    public void checkForCorrectness_exception() {
+        String input = "(s)";
         Assert.assertTrue(stringChecker.checkForCorrectness(input));
     }
 
     @Test
-    public void shouldReturnTrueIfStringHasOpenAndClosedBrackets() {
-        String input = "([{}])";
+    @DisplayName("Should return true if string has open and close brackets")
+    public void checkForOpenClosedBrackets_nominal() {
+        Assert.assertTrue(stringChecker.checkForOpenClosedBrackets(input));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @DisplayName("Should throw exception if string has only open brackets")
+    public void checkForOpenClosedBrackets_exception() {
+        String input = "(((";
         Assert.assertTrue(stringChecker.checkForOpenClosedBrackets(input));
     }
 
